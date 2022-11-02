f167299d16d72abcf10a209bee8ce3f9f4282838
Arnob <arnobdam@gmail.com>
2022-11-02 12:16:16 -0400

    five specs down

--- skeleton/src/piece.js
+++ skeleton/src/piece.js
@@ -1,19 +1,22 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    return ((this.color === "black") ? "white" : "black");
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    return ((this.color === "black") ? this.color = "white" : this.color = "black");
 };
 
 /**
@@ -21,10 +24,15 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    if (this.color === "black") {
+        return "B"
+    } else {
+        return "W"
+    };
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
+if (typeof window === 'undefined') {
     module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
